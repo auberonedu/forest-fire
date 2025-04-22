1d2d7f8277bf191fe8bf8a24b0b17ca53412dce6
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-22 14:30:11 -0700

    First attempt before testing

--- src/Fire.java
+++ src/Fire.java
@@ -1,19 +1,25 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Fire {
     /**
      * Returns how long it takes for all vulnerable trees to be set on fire if a
      * match is lit at a given location.
      * 
-     * The forest is represented via a rectangular 2d char array where t represents a tree
+     * The forest is represented via a rectangular 2d char array where t represents
+     * a tree
      * and . represents an empty space.
      * 
-     * At time 0, the tree at location [matchR, matchC] is set on fire. At every subsequent
-     * time step, any tree that is adjacent (up/down/left/right) to a burning tree is also
-     * set on fire. 
+     * At time 0, the tree at location [matchR, matchC] is set on fire. At every
+     * subsequent
+     * time step, any tree that is adjacent (up/down/left/right) to a burning tree
+     * is also
+     * set on fire.
      * 
      * 
-     * EXAMPLE 
+     * EXAMPLE
      * forest:
      * 
      * t..tttt.t
@@ -27,12 +33,16 @@ public class Fire {
      * Result: 8
      * 
      * Explanation:
-     * At time 0, the tree at (2, 6) is set on fire. At time 1, its adjacent trees also catch on fire
-     * At time 2, the trees adjacent to those catch as well. At time 8, the last tree to catch is at
-     * (0,6). In this example, there is one tree that never burns, so it is not included in the time calculation.
+     * At time 0, the tree at (2, 6) is set on fire. At time 1, its adjacent trees
+     * also catch on fire
+     * At time 2, the trees adjacent to those catch as well. At time 8, the last
+     * tree to catch is at
+     * (0,6). In this example, there is one tree that never burns, so it is not
+     * included in the time calculation.
      * 
      * 
-     * @param forest a 2d array where t represents a tree and . represents the ground
+     * @param forest a 2d array where t represents a tree and . represents the
+     *               ground
      * @param matchR The row the match is lit at
      * @param matchC The column the match is lit at
      * @return the time at which the final tree to be incinerated starts burning
@@ -40,14 +50,62 @@ public class Fire {
     public static int timeToBurn(char[][] forest, int matchR, int matchC) {
         // HINT: when adding to your BFS queue, you can include more information than
         // just a location. What other information might be useful?
-        return -1;
 
-        Queue queue = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{matchR, matchR});
+
+        boolean[][] visited = new boolean[forest.length][forest[0].length];
+
+        int burningTime = 0;
+
+        while(!queue.isEmpty()) {
+            burningTime++;
+            int[] current = queue.poll();
+
+            int currentMatchR = current[0];
+            int currentMatchC = current[1];
+
+            if(visited[currentMatchR][currentMatchC]) {
+                continue;
+            }
+
+            visited[currentMatchR][currentMatchC] = true;
+
+            List<int[]> nextTree = possibleMoves(forest, currentMatchR, currentMatchC, visited);
+
+            queue.addAll(nextTree);
+        }
+
+
+        return burningTime;
+
+        
     }
 
-    private static List<int[]> possibleMoves(char[][] forest, int matchR, int marchC, boolean[][] visited) {
+    private static List<int[]> possibleMoves(char[][] forest, int matchR, int matchC, boolean[][] visited) {
        
-       
-        return null;
+       List<int[]> moves = new ArrayList<>();
+
+       int[][] steps = {
+        {1, 0},
+        {-1, 0},
+        {0, 1},
+        {0, -1}
+       };
+
+        for(int[] step : steps) {
+            int newMatchR = matchR + step[0];
+            int newMatchC = matchC + step[1];
+        
+
+            if(newMatchR >= 0 && newMatchR < forest.length &&
+                newMatchC >= 0 && newMatchC < forest[0].length &&
+                forest[newMatchR][newMatchC] != '.') {
+                    moves.add(new int[]{newMatchR, newMatchC});
+                }
+
+            }
+
+        return moves;
     }
 }
