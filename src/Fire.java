141e73a2a999ce676bbebfcf441330ef1108c851
RMarx1456 <hellohi12465@gmail.com>
2025-04-22 14:35:54 -0700

    Code that's close to finished

--- src/Fire.java
+++ src/Fire.java
@@ -35,6 +35,8 @@ public class Fire {
      * @param matchC The column the match is lit at
      * @return the time at which the final tree to be incinerated starts burning
      */
+
+     private static int[][] moves =  {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
     public static int timeToBurn(char[][] forest, int matchR, int matchC) {
         // HINT: when adding to your BFS queue, you can include more information than
         // just a location. What other information might be useful?
@@ -43,5 +45,26 @@ public class Fire {
         // add tree location and extra information (time it took to reach)
         // write variables to track the current location of columns and rows
         // potentially an integer array to track current coordinates
+
+        int max = 0; //return (the time it takes for all trees to burn).
+
+        if(forest[matchR][matchC] != 't') {
+            return 0;
+        }
+
+        Deque<int[]> deq = new ArrayDeque<int[]>();
+
+        char[][] seen = new char[forest.length][forest[0].length]; //I don't like the [0].length
+        deq.addLast(new int[]{matchR, matchC, 0});
+        while(deq.size() > 0) {
+            int[] current = deq.removeFirst();
+            if(seen[current[0]][current[1]] != 's')
+            for(int[] move : moves) {
+                deq.addLast(new int[]{current[0]+move[0],current[1]+move[1], current[2]++});
+            }
+            seen[current[0]][current[1]] = 's';
+        }
+
+        return max;
     }
 }
