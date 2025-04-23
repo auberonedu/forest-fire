27bbc9eb2f03e3e61bb5ea7eb3f37de6ce78e74d
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-04-22 18:45:40 -0700

    Added additional tests

--- src/FireTest.java
+++ src/FireTest.java
@@ -20,4 +20,55 @@ public class FireTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testMatchOnEmptyGround() {
+        char[][] forest = {
+            {'.','t','t'},
+            {'t','t','.'},
+            {'.','.','t'}
+        };
+    
+        assertEquals(0, Fire.timeToBurn(forest, 0, 0));
+    }
+
+    @Test
+    public void testSingleTree() {
+        char[][] forest = {
+            {'t'}
+        };
+        
+        assertEquals(0, Fire.timeToBurn(forest, 0, 0));
+    }
+
+    @Test
+    public void testAllTreesConnected2x2() {
+        char[][] forest = {
+            {'t','t'},
+            {'t','t'}
+        };
+        
+        assertEquals(2, Fire.timeToBurn(forest, 0, 0));
+    }
+
+    @Test
+    public void testSingleRow() {
+        char[][] forest = {
+            {'t','t','t','t'}
+        };
+        
+        assertEquals(2, Fire.timeToBurn(forest, 0, 1));
+    }
+
+    @Test
+    public void testUnreachableClusters() {
+        char[][] forest = {
+            {'t','t','.','t'},
+            {'t','.','.','.'},
+            {'.','.','t','t'},
+            {'.','.','t','t'}
+        };
+        
+        assertEquals(1, Fire.timeToBurn(forest, 0, 0));
+    }
 }
