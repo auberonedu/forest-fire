7e241f74c867cc2003975bb27351cb389b97b5ce
dannymccarragher <dannymccarragher@gmail.com>
2025-04-24 12:05:54 -0700

    Wrote 2 tests for testing timeToBurn

--- src/FireTest.java
+++ src/FireTest.java
@@ -20,4 +20,38 @@ public class FireTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test 
+    public void testTimeToBurn_OneSurroundedTree(){
+        char[][] forest = {
+            {'t', '.', 't'},
+            {'.', 't', '.'},
+            {'t', '.', 't'}
+        };
+
+        int matchR = 1;
+        int matchC = 1;
+
+        int expected = 0;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+
+    
+    }
+
+    @Test 
+    public void testTimeToBurn_SingleRowOfTrees(){
+        char[][] forest = {
+            {'t', 't', 't'}
+        };
+
+        int matchR = 0;
+        int matchC = 0;
+
+        int expected = 2;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
 }
