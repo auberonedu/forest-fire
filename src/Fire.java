da3e7a33d60b60636a1fb573b4bcc88c1cf825fb
brittLiban <britt.liban@student.greenriver.edu>
2025-04-26 16:32:28 -0700

    Building out a standards bfs?

--- src/Fire.java
+++ src/Fire.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Fire {
     /**
      * Returns how long it takes for all vulnerable trees to be set on fire if a
@@ -38,6 +41,36 @@ public class Fire {
     public static int timeToBurn(char[][] forest, int matchR, int matchC) {
         // HINT: when adding to your BFS queue, you can include more information than
         // just a location. What other information might be useful?
+
+
+        //add a basecase incase its not a tree
+    if(forest[matchR][matchC] != 't'){
+        return 0;
+    }
+
+    int time =0;
+
+    //first thing to do is 
+    Queue<int[]> queue = new LinkedList<>();
+
+    //add something to the cue to get it started
+
+    queue.add(new int[]{
+        matchR,
+        matchC,
+        // the zero is to indicate what what time this tree burned
+        0
+    });
+
+    while(!queue.isEmpty()){
+        int [] current = queue.poll();
+
+        int row = current[0];
+        int col = current[1];
+        int currentTime = current[2];
+    }
+
+    
         return -1;
     }
 }
